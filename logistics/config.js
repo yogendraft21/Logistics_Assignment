1467f230bf3fc4ed37ef6d75e638b2d46069a948
YOGENDRA SINGH VERMA <110186112+yogendraft21@users.noreply.github.com>
2023-08-27 15:16:39 +0530

    MVC Done

--- logistics/config.js
+++ logistics/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dbUri: 'mongodb://localhost:27017/logistics',
+    dbUri: 'mongodb+srv://yogendra:yogendra@cluster0.r2gbftx.mongodb.net/logistics',
     port: 3000,
   };
   
